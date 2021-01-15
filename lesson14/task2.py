2f300587737069f6b88710d502c8d4aceb167899
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-01-15 18:52:42 +0200

    Сделано задание 2

---
+++ lesson14/task2.py
@@ -0,0 +1,29 @@
+print("Введите первую точку")
+
+x1 = float(input('X: '))
+
+y1 = float(input('Y: '))
+
+print("\nВведите вторую точку")
+
+x2 = float(input('X: '))
+
+y2 = float(input('Y: '))
+
+
+
+x_diff = x1 - x2
+
+y_diff = y1 - y2
+
+if x1 == x2:
+    print('Уравненение прямой равно: ', x1)
+else:
+
+    k = y_diff / x_diff
+
+    b = y2 - k * x2
+    print("Уравнение прямой, проходящей через эти точки:")
+
+    print("y = ", k, " * x + ", b)
+
