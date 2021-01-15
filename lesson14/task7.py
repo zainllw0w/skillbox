62b837c1e4bcb94a85cc43a77bf72b72ff685701
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-01-16 00:06:46 +0200

    Сделано задание 7

---
+++ lesson14/task7.py
@@ -0,0 +1,21 @@
+count = 0
+
+start = int(input('Введите начало года: '))
+end = int(input('Введите конец года: '))
+
+for year in range(start, end + 1):
+    text = '' + str(year)
+    for num in str(year):
+        i = 0
+        while i < 4:
+            if num == text[i]:
+                count += 1
+                i += 1
+            else:
+                i += 1
+        if count == 3:
+            print(year)
+            count = 0
+            break
+        else:
+            count = 0
