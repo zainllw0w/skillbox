c43895d337212867c91abb2b6d1a4ac0167487cc
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-02-06 04:27:40 +0200

    Сделано задание 10

---
+++ lesson16/Home Work/task10.py
@@ -0,0 +1,20 @@
+my_list = []
+sequence = ''
+n = int(input('Введите количество чисел: '))
+
+for _ in range(n):
+    num = int(input('Введите число: '))
+    my_list.append(num)
+    sequence += str(num)
+
+print('\nПоследовательность: ', sequence)
+
+if my_list[-1] == my_list[-2]:
+    print('Нужно дописать чисел: ', n - 2)
+    for i in range(len(my_list) - 3, -1, -1):
+        print(my_list[i])
+else:
+    print('Нужно дописать чисел: ', n - 1)
+    for i in range(len(my_list) - 2, -1, -1):
+        print('Сами числа: ', my_list[i], end='')
+
