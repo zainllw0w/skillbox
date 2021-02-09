0523fadc4c6c565e36f71b64bf347ddb24c8d341
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-02-09 18:12:06 +0200

    Сделана задача 4

---
+++ lessons17/HomeWork/task4.py
@@ -0,0 +1,12 @@
+alphabet = 'abcdefg'
+
+print(alphabet[:])
+print(alphabet[::-1])
+print(alphabet[0::2])
+print(alphabet[1::2])
+print(alphabet[:1])
+print(alphabet[-1:-2:-1])
+print(alphabet[3:4])
+print(alphabet[-3:])
+print(alphabet[3:5])
+print(alphabet[4:2:-1])
