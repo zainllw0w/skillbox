4c8553cad25a1ee643e6a28b64a0d9141af79e06
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-02-25 19:47:25 +0200

    сделано задание 7

--- lessons18/HomeWork/task7.py
+++ lessons18/HomeWork/task7.py
@@ -2,6 +2,12 @@ ip = input('Введите ip адресс: ').split('.')
 print(ip)
 error = False
 
+
+while len(ip) != 4:
+    print('должно быть введено 4 числа разделяющиеся точками!. \nПопробуйте снова!')
+    ip = input('Введите ip адресс: ').split('.')
+
+
 for i in range(len(ip)):
     if ip[i].isdigit():
         if int(ip[i]) < 0 or int(ip[i]) > 255:
@@ -9,7 +15,7 @@ for i in range(len(ip)):
             print('Ошибка в значении', ip[i])
             error = True
     else:
-        print('Буквы не могут быть в ip адрессе!')
+        print('Буквы, запятые не могут быть в ip адрессе!')
         error = True
 if error == False:
     print('Все введено верно!')
