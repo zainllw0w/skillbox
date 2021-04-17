b2d201aad1fe967de1572bd85af6685deb48567c
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-04-17 22:04:22 +0300

    последний тест

--- lessons19/HomeWork/task3.py
+++ lessons19/HomeWork/task3.py
@@ -1,1 +1,58 @@
-еще раз тест
+data = {
+    "address": "0x544444444444",
+    "ETH": {
+        "balance": 444,
+        "totalIn": 444,
+        "totalOut": 4
+    },
+    "count_txs": 2,
+    "tokens": [
+        {
+            "fst_token_info": {
+                "address": "0x44444",
+                "name": "fdf",
+                "decimals": 0,
+                "symbol": "dsfdsf",
+                "total_supply": "3228562189",
+                "owner": "0x44444",
+                "last_updated": 1519022607901,
+                "issuances_count": 0,
+                "holders_count": 137528,
+                "price": False
+            },
+            "balance": 5000,
+            "totalIn": 0,
+            "total_out": 0
+        },
+        {
+            "sec_token_info": {
+                "address": "0x44444",
+                "name": "ggg",
+                "decimals": "2",
+                "symbol": "fff",
+                "total_supply": "250000000000",
+                "owner": "0x44444",
+                "last_updated": 1520452201,
+                "issuances_count": 0,
+                "holders_count": 20707,
+                "price": False
+            },
+            "balance": 500,
+            "totalIn": 0,
+            "total_out": 0
+        }
+    ]
+}
+
+# print(data)
+
+print(data['ETH'])
+data['ETH']['total_diff'] = 100
+print(data['ETH'])
+
+print(data['tokens'][0]['fst_token_info']['name'])
+data['tokens'][0]['fst_token_info']['name'] = 'другое имя'
+print(data['tokens'][0]['fst_token_info']['name'])
+dell = data['tokens'][0]['total_out'].pop()
+print(dell)
+
