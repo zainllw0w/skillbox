d0331088e21f76569cdaa35c97b2ed6bb814e89c
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-04-20 07:11:08 +0300

    сделано задание 4

---
+++ lessons19/HomeWork/task4.py
@@ -0,0 +1,38 @@
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
+
+
+
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+
+
+
+for i in goods:
+    quantity = 0
+    price = 0
+    code = goods[i]
+    for n in store[code]:
+        quantity += n['quantity']
+        price += n['quantity'] * n['price']
+    print(i, 'в количестве', quantity, 'шт, стоимость: ', price )
