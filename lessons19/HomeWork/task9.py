491775879cf6a9aa2d88195cd4a8e3b0c5763169
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-04-23 10:29:19 +0300

    задание 9 (возможно можно сделать легче)

--- lessons19/HomeWork/task9.py
+++ lessons19/HomeWork/task9.py
@@ -1,17 +1,42 @@
+def what_is_key(name):
+    for key in tree:
+        for value in tree[key]:
+            if name == value:
+                return key
+
+
 n = int(input('Введите количество человек: '))
 tree = dict()
+
 
 for i in range(1, n):
     print('Пара номер', i, end=': ')
     ask = input().split()
-
-    if ask[1] in tree.values():
+    human_list = set()
+    many = set()
+    for key in tree:
+        for value in tree[key]:
+            many.add(value)
+    if ask[1] in many:
         for human in tree:
             if ask[1] in tree[human]:
                 index = human
-                tree[index + 1] = tree[human].append(ask[0])
+                if index + 1 in tree:
+                    human_list.update(tree[index + 1])
+                human_list.add(ask[0])
+                tree[index + 1] = human_list
+                break
     else:
+        human_list.add(ask[0])
+        tree[i] = human_list
+        human_list = set()
+        human_list.add(ask[1])
+        tree[i-1] = human_list
+    print(tree)
 
-        tree[i-1] = ask[1]
-        tree[i] = ask[0]
-print(tree)
+many.add(ask[0])
+
+
+for i in sorted(many):
+    height = what_is_key(i)
+    print(i, height)
