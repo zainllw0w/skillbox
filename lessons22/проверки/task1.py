5fea56544826d0eb5ae8204e5af874d0f816982c
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-04-29 19:57:45 +0300

    сделано задание 1

---
+++ lessons22/проверки/task1.py
@@ -0,0 +1,14 @@
+import os
+def finder(path):
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            print('Это папка!')
+        elif os.path.isfile(path):
+            print('это файл!')
+            size = os.path.getsize(path)
+            print(f'размер файла: {size}')
+
+
+path = os.path.abspath(os.path.join('/', 'games', 'steam', 'steam.exe'))
+
+finder(path)
